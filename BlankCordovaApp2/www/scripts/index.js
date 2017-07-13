c4e4994cf1007f923955f550d921a3b7c592f301
erana <erana@DESKTOP-7BLVUIF>
2017-07-13 17:57:05 +0300

    add pdf plugin

--- BlankCordovaApp2/www/scripts/index.js
+++ BlankCordovaApp2/www/scripts/index.js
@@ -3,33 +3,109 @@
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
 
-var client = new WindowsAzure.MobileServiceClient(https://protectorpuzzle.azurewebsites.net);
-var item = { test: 'Item 1', complete: false };
-client.getTable('todoitem').insert(item);
 
-(function () {
-    "use strict";
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+var DEBUG = ((typeof cordova) === 'undefined');
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+function progressShow() {
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    if (!_.isEmpty(window.cordova)) {
+        SpinnerPlugin.activityStart("Generating PDF...", {
+            dimBackground: true
+        });
+    }
+
+};
+
+function progressHide() {
+    if (!_.isEmpty(window.cordova)) {
+        SpinnerPlugin.activityStop();
+    }
+};
+
+function success(msg) {
+    debugger;
+    $('#baseH').html('base64:' + msg.replace('\n', ''));
+    
+    $.post("http://localhost:3000/save", {
+        pdfData: msg
+    })
+        .done(function (data) {
+            alert("Data Loaded: " + data);
+        });
+
+    progressHide();
+};
+
+function failure(err) {
+    console.error('->', err);
+    console.alert('An error has ocurred: ', err);
+
+    progressHide();
+};
+
+var HomeView = Backbone.View.extend({
+
+    initialize: function () {
+        this.$button = this.$el.find('#generate');
+        this.$url = this.$el.find('#url');
+        this.$urlShare = this.$el.find('#url-share');
+        this.$raw = this.$el.find('#rawhtml');
+        this.$html = this.$el.find('#html');
+        this.$display = this.$el.find('#display');
+
+        this.success = success.bind(this);
+        this.failure = failure.bind(this);
+    },
+
+    events: {
+        'click #generate': 'makePDFBase64',
+    },
+
+    makePDFBase64: function (e) {
+        navigator.notification.alert("sldkasdlas");
+        e.preventDefault();
+        progressShow();
+        /* generate pdf using url. */
+        pdf.htmlToPDF({
+            url: "http:\\www.google.com",
+            documentSize: "A4",
+            landscape: "portrait",
+            type: "base64"
+        }, this.success, this.failure);
+    },
+});
+
+var DemoRouter = Backbone.Router.extend({
+    routes: {
+        '*path': 'index',
+    },
+
+    index: function () {
+        new HomeView({
+            el: $('.starter-template')
+        });
+    }
+});
+
+
+if (DEBUG) {
+    console.log('start app..');
+
+    new DemoRouter();
+    Backbone.history.start();
+} else {
+    document.addEventListener('deviceready', function () {
+
+        console.log('start app..');
+
+        new DemoRouter();
+        Backbone.history.start();
+    }, false);
+
+}
+
